b55bf5d37e2bd2e986816526ee59de92da25267f
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-05-08 00:15:49 +0200

    feat: implement area calculation for building, level and room
    Closes #2

--- src/main/java/pl/put/rest/BuildingInfoController.java
+++ src/main/java/pl/put/rest/BuildingInfoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-RestController
+@RestController
 @RequestMapping("/api/building")
 public class BuildingInfoController {
 
